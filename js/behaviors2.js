bd0f3e6b385448ba1f5b61b53fedb3d09170b128
Gotham <gotham@thesourcewall.com>
2015-08-05 21:30:23 -0400

    Add old JS back in as behaviors 2 (for reference)

--- js/behaviors2.js
+++ js/behaviors2.js
@@ -1,7 +1,57 @@
+$(function() {
+
+  // initialize Isotope
+  var $container = $('.js-isotope').isotope({
+    itemSelector: '.person',
+    layoutMode: 'masonry',
+    getSortData: {
+      name: '.person__name',
+      floor: '[data-floor]'
+    },
+    sortAscending: {
+      name: true,
+      floor: false
+    },
+    sortBy: 'random'
+  });
+
+  // filter on click
+  $('#filters li').on('click', function() {
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  });
+
+  // sort on click
+  $('#sorts li').on('click', function() {
+    var sortValue = $(this).attr('data-sort');
+    $container.isotope({ sortBy: sortValue });
+  });
+
+  // show and hide cards
+  $('.person').on('click', function() {
+    $('.cards').fadeIn();
+  });
+  $('.card__close').on('click', function() {
+    $('.cards').fadeOut();
+  });
+
+});
+
+// adding captions to Flickity
+// http://codepen.io/desandro/pen/dPdVNM
+
 $(document).ready( function() {
-  $(".site-nav a").click( function() {
-    console.log("CLICKIN'");
-    $(".active").toggleClass("active");
-    $("#"+ this.dataset.targetPage).toggleClass("active");
+  var $gallery = $('.gallery').flickity({
+    cellSelector: 'img',
+    imagesLoaded: true,
+    percentPosition: false
+  });
+  var $caption = $('.caption');
+  // Flickity instance
+  var flkty = $gallery.data('flickity');
+
+  $gallery.on( 'cellSelect', function() {
+    // set image caption using img's alt
+    $caption.text( flkty.selectedElement.alt );
   });
 });
